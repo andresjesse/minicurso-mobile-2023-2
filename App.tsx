9b095f327bcb4d38e4bd5eb66c8be2209885714d
Andres <andres.porfirio.tablet@gmail.com>
2023-10-17 15:01:44 -0300

    passo 2

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Serie from "./components/Serie";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>My Series</Text>
       <StatusBar style="auto" />
+
+      <Serie />
+      <Serie />
+      <Serie />
+      <Serie />
     </View>
   );
 }
--- components/Serie.tsx
+++ components/Serie.tsx
@@ -1,10 +1,25 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import React, { useState } from "react";
 
 export default function Serie() {
+  const [season, setSeason] = useState(0);
+
   return (
-    <View>
-      <Text>Serie</Text>
+    <View style={styles.container}>
+      <Button title="-" onPress={() => setSeason(season - 1)} />
+      <Text style={styles.text}>{season}</Text>
+      <Button title="+" onPress={() => setSeason(season + 1)} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+  },
+  text: {
+    fontSize: 32,
+    width: 50,
+    textAlign: "center",
+  },
+});
